87ca7f110fcbdfe689ceb1347eb3b7b207537eef
Marcin Lewandowski <marcin.lewandowski@avicon.pl>
2021-03-26 13:28:57 +0100

    force mp4 output container

--- piotr-eiti/studia-compress.py
+++ piotr-eiti/studia-compress.py
@@ -9,6 +9,8 @@ files = [f for ext in exts for f in glob.glob(os.path.join(r'./*/*/', ext))]
 
 for in_f in files:
     out_f = in_f.replace('ł', 'l').replace('\\Wyklady\\', '-')
+    for ext in exts:
+        out_f = out_f.replace(ext[1:], '.mp4')
     print(f'{in_f} -> {out_f}')
     subprocess.call(f'ffmpeg -i {in_f} -ac 1 -vsync vfr -vf mpdecimate,scale=iw/2:ih/2 {out_f}', shell=True)
 
