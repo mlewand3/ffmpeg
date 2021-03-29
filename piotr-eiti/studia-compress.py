063eea950400128de9449efd91c8493ed0e85d77
Marcin Lewandowski <marcin.lewandowski@avicon.pl>
2021-03-29 14:56:23 +0200

    info about NUC

--- piotr-eiti/studia-compress.py
+++ piotr-eiti/studia-compress.py
@@ -1,8 +1,14 @@
+# To call python/cmd/ps with NUC, please use:
+# pushd "\\172.16.0.110\AVICON wymiana\DLA PIOTRKA\Studia"
+# See more: https://superuser.com/questions/282963/browse-an-unc-path-using-windows-cmd-without-mapping-it-to-a-network-drive
+
 import os
 import glob
 import subprocess
 
-os.chdir(r'C:\Users\AVICON\Desktop\Studia')
+#os.chdir(r'C:\Users\AVICON\Desktop\Studia')
+out_dir = 'Compressed'
+os.makedirs(out_dir, exist_ok=True)
 
 exts = ['*.mkv', '*.avi', '*.mp4']
 files = [f for ext in exts for f in glob.glob(os.path.join(r'./*/*/', ext))]
@@ -11,6 +17,8 @@ for in_f in files:
     out_f = in_f.replace('ł', 'l').replace('\\Wyklady\\', '-')
     for ext in exts:
         out_f = out_f.replace(ext[1:], '.mp4')
-    print(f'{in_f} -> {out_f}')
-    subprocess.call(f'ffmpeg -n -i "{in_f}" -ac 1 -vsync vfr -vf mpdecimate,scale=iw/2:ih/2 "{out_f}"', shell=True)
+    out_f = r'./' + out_dir + r'/' + out_f[2:]
 
+    print(f'{in_f} -> {out_f}')
+
+    subprocess.call(f'ffmpeg -n -i "{in_f}" -ac 1 -vsync vfr -vf mpdecimate,scale=iw/2:ih/2 "{out_f}"', shell=True)
